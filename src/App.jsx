7631f5cab678b7365b5caebf582461ed42d9df60
Yashwanth Kumar <yash.madenenni@outlook.com>
2024-01-03 18:43:04 +0000

    Folder Structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-const App = () => {
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
+
+const App = () => (
      <BrowserRouter>
      <Box sx={{ backgroundColor: '#000'}}>
-        Navbar
+        <Navbar />
         <Routes>
             <Route path='/' exact element={<Feed />} />
-            Route
+            <Route path='/video/:id' element={<VideoDetail />} />
+            <Route path='/channel/:id' element={<ChannelDetail />} />
+            <Route path='/search/:searchTerm' element={<SearchFeed />} />
         </Routes>
      </Box>
      </BrowserRouter>
-}
+);
 
 export default App;
